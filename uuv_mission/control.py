6190907aa1fe1664740df74285fc2f743568ee98
Nathan Patel <nathanpatel.mailbox@gmail.com>
2025-10-19 12:56:53 +0100

    feat(control): improve comments

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,12 +1,21 @@
 # control.py
+# Simple PD controller used by the cave mission simulation.
+# PD = Proportional + Derivative. We store the previous error to estimate the derivative term.
 
-# PD controller per task (defaults: Kp = 0.15, Kd = 0.6)
 class PDController:
     def __init__(self, kp: float = 0.15, kd: float = 0.6):
+        
+        """
+        Args:
+            kp: Proportional gain (how strongly we react to current error)
+            kd: Derivative gain (how strongly we react to change in error)
+        """
         self.kp = kp
         self.kd = kd
-        self.prev_e = 0.0  # remember e[t-1]
+        
+        # Store previous error e[t-1] so we can compute the derivative term.
+        self.prev_e = 0.0 
 
-    # to clear previous error
+    # Clear controller memory (previous error). Call before a new run.
     def reset(self):
         self.prev_e = 0.0
